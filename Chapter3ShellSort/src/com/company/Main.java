63225fbe1b265d98166098df81db03462fcd1553
DESKTOP-9DRHSQH\matei <matei.lazar@protonmail.com>
2021-03-03 22:29:29 +0200

    chapter 3 factorial iterative and recursion

--- Chapter3ShellSort/src/com/company/Main.java
+++ Chapter3ShellSort/src/com/company/Main.java
@@ -24,14 +24,4 @@ public class Main {
 
         for (int j : array) System.out.println(j);
     }
-
-    public static void swap(int[] array, int i, int j) {
-        if (i==j) {
-            return;
-        }
-
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
 }
