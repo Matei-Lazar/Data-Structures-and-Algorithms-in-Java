967ebbffce9b2e4edbedbf56310f2e5b09029997
DESKTOP-9DRHSQH\matei <matei.lazar@protonmail.com>
2021-03-17 19:54:09 +0200

    added a recursive method for insertion sort

--- Chapter3InsertionSort/src/com/company/Main.java
+++ Chapter3InsertionSort/src/com/company/Main.java
@@ -6,6 +6,13 @@ public class Main {
 
         int[] array = {20, 35, -15, 7, 55, 1, -22};
 
+        //insertionSort(array);
+        insertionSortRecursive(array, array.length);
+
+        for (int j : array) System.out.println(j);
+    }
+
+    public static void insertionSort(int[] array) {
         for (int firstUnsortedIndex = 1; firstUnsortedIndex < array.length; firstUnsortedIndex++) {
 
             int newElement = array[firstUnsortedIndex];
@@ -17,8 +24,22 @@ public class Main {
             }
             array[i] = newElement;
         }
-
-        for (int j : array) System.out.println(j);
     }
-    
+
+    public static void insertionSortRecursive(int[] array, int numItems) {
+        if (numItems < 2) {
+            return;
+        }
+
+        insertionSortRecursive(array, numItems-1);
+
+        int newElement = array[numItems - 1];
+
+        int i;
+
+        for (i = numItems - 1; i > 0 && array[i-1] > newElement; i--) {
+            array[i] = array[i-1];
+        }
+        array[i] = newElement;
+    }
 }
