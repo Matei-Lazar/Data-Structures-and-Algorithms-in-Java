82a4ca9f91a99daa4d76ec57afae8bba36255058
DESKTOP-9DRHSQH\matei <matei.lazar@protonmail.com>
2021-03-17 22:03:01 +0200

    added methods for sorting strings

--- Chapter3RadixSort/src/com/company/Main.java
+++ Chapter3RadixSort/src/com/company/Main.java
@@ -4,10 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         int[] radixArray = {4725, 4586, 1330, 8792, 1594, 5729};
+        String[] stringArray = {"bcdef", "dbaqc", "abcde", "omadd", "bbbbb"};
 
         radixSort(radixArray, 10, 4);
+        radixSortString(stringArray, 26, 5);
 
         for (int item : radixArray) {
+            System.out.println(item);
+        }
+        for (String item : stringArray) {
             System.out.println(item);
         }
     }
@@ -42,5 +47,37 @@ public class Main {
 
     public static int getDigit(int position, int value, int radix) {
         return value / (int) Math.pow(radix, position) % radix;
+    }
+
+    public static void radixSortString(String[] input, int radix, int width) {
+        for (int i = width - 1; i >= 0; i--) {
+            radixSingleSortString(input, i, radix);
+        }
+    }
+
+    public static void radixSingleSortString(String[] input, int position, int radix) {
+        int numItems = input.length;
+        int[] countArray = new int[radix];
+
+        for (String value: input) {
+            countArray[getLetter(position, value)]++;
+        }
+        // Adjust the count array
+        for (int j = 1; j < radix; j++) {
+            countArray[j] += countArray[j - 1];
+        }
+
+        String[] temp = new String[numItems];
+        for (int tempIndex = numItems -1; tempIndex >= 0; tempIndex--) {
+            temp[--countArray[getLetter(position, input[tempIndex])]] =
+                    input[tempIndex];
+        }
+        for (int tempIndex = 0; tempIndex < numItems; tempIndex++) {
+            input[tempIndex] = temp[tempIndex];
+        }
+    }
+
+    public static int getLetter(int position, String value) {
+        return value.charAt(position) - 'a';
     }
 }
