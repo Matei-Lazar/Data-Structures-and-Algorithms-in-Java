587f46c7478664c532001bbc13a50d48d506ee35
DESKTOP-9DRHSQH\matei <matei.lazar@protonmail.com>
2021-03-25 12:20:16 +0200

    chapter 6 queue implemented using array and optimized as a circular queue

---
+++ Chapter6QueueByArray/src/com/company/ArrayQueue.java
@@ -0,0 +1,114 @@
+package com.company;
+
+import java.util.NoSuchElementException;
+
+public class ArrayQueue {
+
+    private Employee[] queue;
+    private int front;
+    private int back;
+
+    public ArrayQueue(int capacity) {
+        queue = new Employee[capacity];
+    }
+
+    public void add(Employee employee) {
+        if (back == queue.length) {
+            Employee[] newArray = new Employee[2 * queue.length];
+            System.arraycopy(queue, 0, newArray, 0, queue.length);
+            queue = newArray;
+        }
+
+        queue[back] = employee;
+        back++;
+    }
+    // using an array with a capacity of 5, if we only ever have 2 people in the queue, but keep pshing and popping
+    // people alternatively,we'll reach the end of the array and most of it it's empty.
+    public void addOptimized(Employee employee) {
+        if (size() == queue.length - 1) {
+            int numItems = size();
+            Employee[] newArray = new Employee[2 * queue.length];
+
+            System.arraycopy(queue, front, newArray, 0, queue.length - front);
+            System.arraycopy(queue, 0, newArray, queue.length - front, back);
+
+            queue = newArray;
+
+            front = 0;
+            back = numItems;
+        }
+
+        queue[back] = employee;
+        if (back < queue.length - 1) {
+            back++;
+        } else {
+            back = 0;
+        }
+    }
+
+    public Employee remove() {
+        if (size() == 0) {
+            throw new NoSuchElementException();
+        }
+
+        Employee employee = queue[front];
+        queue[front] = null;
+        front++;
+        if (size() == 0) {
+            front = 0;
+            back = 0;
+        }
+
+        return employee;
+    }
+
+    public Employee removeOptimized() {
+        if (size() == 0) {
+            throw new NoSuchElementException();
+        }
+
+        Employee employee = queue[front];
+        queue[front] = null;
+        front++;
+        if (size() == 0) {
+            front = 0;
+            back = 0;
+        } else if (front == queue.length) {
+            front = 0;
+        }
+
+        return employee;
+    }
+
+    public Employee peek() {
+        if (size() == 0) {
+            throw new NoSuchElementException();
+        }
+
+        return queue[front];
+    }
+
+    public int size() {
+        if (front <= back) {
+            return back - front;
+        } else {
+            return back - front + queue.length;
+        }
+    }
+
+    public void printQueue() {
+        if (front <= back) {
+            for (int i = front; i < back; i++) {
+                System.out.println(queue[i]);
+            }
+        } else {
+            for (int i = front; i < queue.length; i++) {
+                System.out.println(queue[i]);
+            }
+            for (int i = 0; i < back; i++) {
+                System.out.println(queue[i]);
+            }
+        }
+    }
+
+}
